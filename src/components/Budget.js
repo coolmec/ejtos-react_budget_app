1a4295e0d50c3ae3b07fda14b8b835d1d529104b
jmoukaila <jmoukaila@yahoo.fr>
2023-04-12 12:44:12 -0400

    Updated Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,24 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, expense) =>{
+        return total+=expense.cost;
+    },0);
+    const updateBudget = (newBudget) => {
+        if(newBudget < totalExpenses) {
+            alert("Budget can't be lower than spendings !");
+        }
+        else {
+            dispatch({type:'SET_BUDGET', payload: newBudget});
+        }
+
+    };
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <input type="number" 
+            max="20000" step="10" value={budget}
+            onChange={(e)=>{updateBudget(e.target.value)}}></input>
         </div>
     )
 };
